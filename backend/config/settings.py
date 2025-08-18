a56bbae0405d4c0885363b4cf36cac831d5e10ad
Nanjiba Jahin Sadique <126199401+Nanji03@users.noreply.github.com>
2025-08-18 14:59:53 -0400

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "NoteSync-backend.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "NoteSync-backend1.onrender.com"]
 
 # Application definition
 
